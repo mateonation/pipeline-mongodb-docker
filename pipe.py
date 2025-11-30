d6855f99e845980c6ecd3651a18b3e5913df654f
franpelirrojo <franfc.00@gmail.com>
2025-11-30 06:37:01 +0100

    feat: script automático cassandra

--- pipe.py
+++ pipe.py
@@ -1,21 +1,25 @@
 from cassandra.cluster import Cluster
 import mysql.connector
 import pandas as pd
+import time
+from itertools import count
 
 DEBUG = True
 
 def main():
+    start_time = time.time()
     df = pd.read_csv('spotify-clean.csv')
+    if DEBUG: print(time.time() - start_time)
 
-    cnx = mysql.connector.connect(
+    cnx_mysql = mysql.connector.connect(
         host="localhost",
         user="root",
         password="changeme",
         database="pipe"
     )
-    cursor = cnx.cursor()
+    cursor = cnx_mysql.cursor()
     
-    if DEBUG: cursor.execute("DROP TABLE IF EXISTS track")
+    cursor.execute("DROP TABLE IF EXISTS track")
 
     cursor.execute("CREATE TABLE IF NOT EXISTS track (" \
         "track_id varchar(250), " \
@@ -25,11 +29,43 @@ def main():
         "popularity int," \
         "duration_ms bigint," \
         "PRIMARY KEY (track_id)" \
-        ");")
+        ")")
 
     sql = "INSERT INTO track VALUES (%s, %s, %s, %s, %s, %s)"
     tuples=[x for x in df.itertuples(index=False, name=None)]
     cursor.executemany(sql, tuples)
-    cnx.commit()
+    cnx_mysql.commit()
 
-main()
+    if DEBUG: print(time.time() - start_time)
+    cursor.execute("SELECT * FROM track")
+    rows = cursor.fetchall()
+    df = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
+
+    session=Cluster(['localhost'], port=9042).connect() 
+    session.execute("""CREATE KEYSPACE IF NOT EXISTS pipe WITH replication = {'class': 'SimpleStrategy', 'replication_factor':1};""")
+    session.execute('USE pipe')
+
+    session.execute("DROP TABLE IF EXISTS track")
+
+    session.execute("CREATE TABLE IF NOT EXISTS track ("\
+        "track_id VARCHAR PRIMARY KEY,"\
+        "artists VARCHAR,"\
+        "album_name VARCHAR,"\
+        "track_name VARCHAR,"\
+        "popularity INT,"\
+        "duration_ms BIGINT)")
+
+    ps = session.prepare("INSERT INTO track ("\
+        "track_id, artists, album_name, track_name, popularity, duration_ms)"\
+        "VALUES (?, ?, ?, ?, ?, ?)")
+
+    rows=list(df.itertuples(index=False, name=None))
+    futueres = session.execute_concurrent_with_args(ps, tuples)
+
+    if DEBUG: print(time.time() - start_time)
+
+    result=session.execute("SELECT * FROM track")
+    errors = [r for r in futueres if not r[0]]
+    print(f"Inserciones en cassandra: {len(result.all())}\nErrores totales: {len(errors)}")
+
+if __name__ == "__main__": main()
